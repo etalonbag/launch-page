266ddc459fb0b544c9a309dfa341937f753ddcd9
burdiyan <burdiyan@gmail.com>
2015-03-02 15:27:08 +0100

    Styles, mixpanel, images, form

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,10 +1,13 @@
 var gulp = require('gulp');
 var less = require('gulp-less');
+var notify = require('gulp-notify');
+
 
 gulp.task('less', function() {
     gulp.src('./public/assets/less/main.less')
         .pipe(less())
-        .pipe(gulp.dest('./public/assets/css/'));
+        .pipe(gulp.dest('./public/assets/css/'))
+        .pipe(notify('Less complied'));
 });
 
 gulp.task('watch:less', ['less'], function() {
