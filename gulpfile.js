cad9a9508055f61c4c943de49ef75664aa223d67
burdiyan <burdiyan@gmail.com>
2015-03-19 12:49:35 +0100

    js concat

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 var gulp = require('gulp');
 var less = require('gulp-less');
 var notify = require('gulp-notify');
-
+var concat = require('gulp-concat');
 
 gulp.task('less', function() {
     gulp.src('./public/assets/less/main.less')
@@ -12,6 +12,14 @@ gulp.task('less', function() {
 
 gulp.task('watch', ['less'], function() {
    gulp.watch('./public/**/*.less', ['less']);
+});
+
+gulp.task('js', function() {
+    gulp.src([
+        './public/components/jquery/dist/jquery.min.js',
+        './public/components/social-likes/social-likes.min.js',
+    ]).pipe(concat('bundle.js'))
+        .pipe(gulp.dest('./public/assets/'));
 });
 
 gulp.task('default', ['less']);
